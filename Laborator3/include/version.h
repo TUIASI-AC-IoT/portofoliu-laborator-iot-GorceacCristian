315aa6416aed6cf629fe9c70c83e8592507f7fef
GorceacCristian <102883328+GorceacCristian@users.noreply.github.com>
2025-03-03 19:34:31 +0200

    Add files via upload
    Laborator 3

---
+++ Laborator3/include/version.h
@@ -0,0 +1,10 @@
+
+#ifndef BUILD_NUMBER
+  #define BUILD_NUMBER "4"
+#endif
+#ifndef VERSION
+  #define VERSION "v0.1.4 - 2025-03-03 19:25:56.933182"
+#endif
+#ifndef VERSION_SHORT
+  #define VERSION_SHORT "v0.1.4"
+#endif
